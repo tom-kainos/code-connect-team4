99e7d260548b6d76e2bc834d9961642a8e935369
liam nesbitt <liam.wrote.this@hotmail.co.uk>
2026-05-18 15:39:11 +0100

    employee details edit button functionality added

--- public/javascripts/employeeDetail.js
+++ public/javascripts/employeeDetail.js
@@ -1,7 +1,7 @@
-// Get employee ID from button's data attribute
+// Redirect to the update employee route when Edit is clicked
 document.getElementById('editBtn').addEventListener('click', function () {
     const empId = this.getAttribute('data-id');
-    alert('Edit feature coming soon!\nEmployee ID: ' + empId);
+    window.location.href = '/employees/update/' + empId;
 });
 
 document.getElementById('deleteBtn').addEventListener('click', function () {
